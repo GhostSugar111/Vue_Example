0a9113a65ffa5df9720fa1ff5b70193ec9bc3b6d
XuHan <1178263698@qq.com>
2023-03-29 09:34:00 +0800

    notebook

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <test1></test1>
-</template>
+  <!-- <test1></test1> -->
+  <notebook></notebook>
+  <!-- <button v-on:click="handleClick('参数')">测试</button>
+  <button @click.stop="handleClick('参数',$event)">测试</button> -->
 
+  <!-- <a href="https://www.baidu.com" target="_blank" >测试</a>
+  <a href="https://www.baidu.com" target="_blank" @click="handleClick('参数',$event)">测试</a>
+  <a href="https://www.baidu.com" target="_blank" @click.prevent="handleClick('参数',$event)">测试</a> -->
+  <!-- <input v-on:keyup.13="submit" >
+  <input v-on:keyup.enter="submit" autofocus >
+  <input @keyup.ctrl="submit" autofocus> -->
+  
+</template>
+<script setup>
+import test1 from './components/test1.vue'
+import notebook from './components/NoteBook.vue'
+// import { reactive, toRefs } from 'vue'
+
+</script>
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import test1 from './components/test1.vue'
 
-export default {
-  name: 'App',
-  components: {
-    // HelloWorld,
-    test1
-  }
-}
+// export default {
+  // name: 'App',
+  // methods: {
+  //   submit() {
+  //     console.log('submit')
+  //     alert(1)
+  //   },
+  // },
+
+  // components: {
+  //   // HelloWorld,
+    // test1
+
+  // }
+
+// }
 </script>
 
 <style>
@@ -22,7 +46,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   /* margin-top: 60px; */
 }
---
+++ src/components/NoteBook.vue
@@ -0,0 +1,69 @@
+//实验三 简易记事本
+<template>
+    <!-- <div>记事本</div> -->
+    <h1>记事本</h1>
+    <input type="text" v-model.trim="input" id="addinput" @keyup.enter="add">
+    <!-- <button @click="add">添加</button> -->
+    <div v-for="(item, index) in list" :key="index">
+        <p>
+            <span id="count">{{ index + 1 }}</span>
+            <span id="title">{{ item}}</span>
+            <button id="delete" @click="del(index)">删除</button>
+        </p>
+    </div>
+    <div style="margin-left: 50px;" v-if="list.length > 0">
+        <span style="margin-right: 40%;">条数：{{ list.length }} </span>
+        
+        <button @click="clear">清除所有记录</button>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+const input = ref("Hello Vue 3.0 World!")
+const list = ref([])
+const isDisplay=ref(0)
+// const count=ref(0)
+function add() {
+    // console.log(input.value);
+    if (input.value != "Hello Vue 3.0 World!" && input.value != "") {
+        list.value.push(input.value)
+        input.value=""
+    }
+}
+function del(index){
+    // console.log(index);
+    list.value.splice(index,1)
+}
+function clear(){
+    list.value=[]
+}
+</script>
+<style scoped>
+#count {
+    display: inline-block;
+    width: 30px;
+    /* height: 30px; */
+    text-align: center;
+    background-color: #ccc;
+    line-height: 30px;
+    margin-right: 10px;
+    border-radius: 10px;
+}
+#title{
+
+}
+#delete {
+    display: inline-block;
+    margin-left: auto;
+}
+p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 50px;
+    height: 50px;
+    border-bottom: 1px solid black
+}
+
+
+</style>
