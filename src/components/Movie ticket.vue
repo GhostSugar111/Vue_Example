ce994707b016a66c26a341a9037b57a5f1700655
XuHan <1178263698@qq.com>
2023-04-10 22:30:50 +0800

    axios

--- src/components/Movie ticket.vue
+++ src/components/Movie ticket.vue
@@ -35,8 +35,18 @@
             <div class="rightBottom">
                 <!-- 电影票的购买信息-->
                 <p>电影院：<strong>{{ filmInfo.cinema }}</strong></p>
-                <p>影厅：<strong>{{ filmInfo.room }}</strong></p>
-                <p>场次：<strong>{{ filmInfo.time }}</strong></p>
+                <p>影厅：
+                    <select v-model="selectedHall" @change="load">
+                        <option disabled value="">请选择影厅</option>
+                        <option v-for="hall in halls" :value="hall.id">{{ hall.name }}</option>
+                    </select>
+                </p>
+                <p>场次：
+                    <select v-model="selectedTime" @change="loadSeats">
+                        <option disabled value="">请选择场次</option>
+                        <option v-for="time in times[selectedHall - 1]" :value="time.id">{{ time.name }}</option>
+                    </select>
+                </p>
                 <p id="seatSelect">座位：<span v-for="(item, index) in curSeatDisp" :key="index">{{ item }}</span></p>
                 <p>已选择
                     <strong style="color: red;">{{ count }}
@@ -50,17 +60,28 @@
                 <p>总价：<strong>￥{{ numberFormat(fileTotal) }}</strong></p>
                 <hr>
                 <button type="button" class="btn" @click="handleBuy" style="display: block; margin: 0 auto;">确定下单</button>
+                <h2 style="color: red;" v-if="fullFlag"> 座位已满！</h2>
             </div>
         </div>
     </div>
 </template>
 <script>
 import { reactive, toRefs, computed } from "vue";
+import axios from 'axios'
 export default {
     setup() {
+        const fullFlag = computed(() => {
+            if (!state.seatflag.includes(0)) {
+                return true;
+            } else {
+                return false;
+            }
+        })
         const state = reactive(
             {
                 seatflag: [
+                    -1, -1, 0, 0, 0, 0, 0, 0, -1, -1,
+                    -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -69,13 +90,44 @@ export default {
                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 2, 2, 0, 0, 0, 0,
-                    0, 0, 0, 2, 2, 0, 2, 2, 0, 0,
                     -1, 0, 0, 0, 0, 0, 0, 0, 0, -1,
                     -1, -1, 0, 0, 0, 0, 0, 0, -1, -1,
                 ],
+                selectedHall: '',
+                selectedTime: '',
+                halls: [
+                    { id: 1, name: '1号厅' },
+                    { id: 2, name: '2号厅' },
+                    { id: 3, name: '3号厅' },
+                ],
+                times: [
+
+                    [
+                        { id: 1, name: '2023.04.08(周六) 20:00' },
+                        { id: 2, name: '2023.04.09(周日) 18:00' },
+                        { id: 3, name: '2023.04.10(周一) 21:00' }
+                    ]
+                    ,
+                    [
+
+                        { id: 4, name: '2023.04.05(周三) 20:00' },
+                        { id: 5, name: '2023.04.06(周四) 22:00' },
+                        { id: 6, name: '2023.04.07(周五) 21:00' },
+                    ]
+                    , [
+
+                        { id: 7, name: '2023.04.02(周日) 20:00' },
+                        { id: 8, name: '2023.04.03(周一) 22:00' },
+                        { id: 9, name: '2023.04.04(周二) 20:00' },
+                    ]
+
+
+
+                ],
+                // seatflag: [],
                 curSeat: [],
                 curSeatDisp: [],
+                alert: '',
                 count: 0,
                 maxFlag: false,
                 filmInfo: {
@@ -94,6 +146,135 @@ export default {
                 }
             }
         )
+        const load = () => {
+            if (state.selectedHall == 1) {
+                state.selectedTime = 1
+                state.filmInfo.unitPrice = 38
+                axios.get('seat.json').then(response => {
+                    console.log('Seat data:', response.data);
+                    state.seatflag = response.data.seat1
+                }).catch(error => {
+                    console.error('Error loading seat data:', error)
+                })
+
+            } else if (state.selectedHall == 2) {
+                state.selectedTime = 4
+                state.filmInfo.unitPrice = 38
+                axios.get('seat.json').then(response => {
+                    console.log('Seat data:', response.data);
+                    state.seatflag = response.data.seat4
+                }).catch(error => {
+                    console.error('Error loading seat data:', error)
+                })
+            } else if (state.selectedHall == 3) {
+                state.selectedTime = 7
+                state.filmInfo.unitPrice = 38
+                axios.get('seat.json').then(response => {
+                    console.log('Seat data:', response.data);
+                    state.seatflag = response.data.seat7
+                }).catch(error => {
+                    console.error('Error loading seat data:', error)
+                })
+            }
+        }
+        const loadSeats = () => {
+            console.log(state.selectedHall, state.selectedTime);
+            if (state.selectedHall == 1) {
+                switch (state.selectedTime) {
+                    case 1:
+                        state.filmInfo.unitPrice = 38
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat1
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+                    case 2:
+                        state.filmInfo.unitPrice = 58
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat2
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+                    case 3:
+                        state.filmInfo.unitPrice = 78
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat3
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+
+                }
+
+            } else if (state.selectedHall == 2) {
+                switch (state.selectedTime) {
+                    case 4:
+                        state.filmInfo.unitPrice = 38
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat4
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+                    case 5:
+                        state.filmInfo.unitPrice = 58
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat5
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+                    case 6:
+                        state.filmInfo.unitPrice = 78
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat6
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+
+                }
+            } else if (state.selectedHall == 3) {
+                switch (state.selectedTime) {
+                    case 7:
+                        state.filmInfo.unitPrice = 38
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat7
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+                    case 8:
+                        state.filmInfo.unitPrice = 58
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat8
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+                    case 9:
+                        state.filmInfo.unitPrice = 78
+                        axios.get('seat.json').then(response => {
+                            console.log('Seat data:', response.data);
+                            state.seatflag = response.data.seat9
+                        }).catch(error => {
+                            console.error('Error loading seat data:', error)
+                        })
+                        break;
+
+                }
+            }
+        }
         const handleClick = (index) => {
             if (state.seatflag[index] == 1) {
                 state.seatflag[index] = 0;
@@ -118,28 +299,40 @@ export default {
                 state.maxFlag = false;
             }
         }
-        const fileTotal=computed(()=>{
-            return state.count*state.filmInfo.unitPrice;
+        const handleBuy = () => {
+            console.log(state.curSeat);
+            for (const data of state.curSeat) {
+                console.log(data);
+                state.seatflag[data] = 2;
+            }
+        }
+        const fileTotal = computed(() => {
+            return state.count * state.filmInfo.unitPrice;
         })
-        const numberFormat=(value)=>{
+        const numberFormat = (value) => {
             return value.toFixed(2);
         }
         return {
             ...toRefs(state),
             handleClick,
             fileTotal,
-            numberFormat
+            numberFormat,
+            loadSeats,
+            load,
+            handleBuy,
+            fullFlag
         }
     }
 }
 </script>
 <style lang="scss" scoped>
-#seatSelect span{
-    border:1px solid ;
+#seatSelect span {
+    border: 1px solid;
     background-color: #fff;
-    margin:5px;
-    padding:1px
+    margin: 5px;
+    padding: 1px
 }
+
 .seat {
     float: left;
     width: 30px;
@@ -177,6 +370,10 @@ export default {
     width: 1050px;
     border: 1px solid grey;
     height: 550px;
+}
+
+.fileTop {
+    margin: 0 auto;
 }
 
 .filmLeft {
